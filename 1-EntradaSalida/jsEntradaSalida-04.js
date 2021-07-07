f5e789b4ec5f0d0b48fa6cae0057b2bfe4c8c468
unknown <laurafilone@gmail.com>
2021-07-06 22:55:43 -0300

    cambios ejer4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,10 +2,14 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+
+
 function mostrar()
 {
+	// Reservo espacio en memoria para guardar el nombre
 	let nombre;
+	// Guardo la variabble
 	nombre = prompt ("Indique su nombre");
-	alert("ok");
+	document.getElementById("txtIdNombre").value = nombre;
 }
 
