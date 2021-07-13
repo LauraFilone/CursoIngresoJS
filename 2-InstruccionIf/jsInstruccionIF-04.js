446d5ce4bdb1f7b3827a93fc32f51369166fe660
unknown <laurafilone@gmail.com>
2021-07-13 19:41:50 -0300

    nuevos cambios

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -4,8 +4,11 @@ function mostrar()
 
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-	if (edad >=13 && edad <=17) {
+	/*if (edad >=13 && edad <=17) {
 			alert ("usted es adolescente");
+	}*/
+	if (!(edad <13 || edad >17)){
+		alert("es adolescente");
 	}
 }
 
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,11 +6,30 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    let largo;
+    let ancho;
+    let perimetro;
+    let alambre;
+
+    largo = parseInt(document.getElementById("txtIdLargo").value);
+    ancho = parseInt(document.getElementById("txtIdAncho").value);
+
+    perimetro = (largo + ancho) * 2;
+    alambre = perimetro*3;
+    alert(" Se necesitan " + alambre + " metros de alambre." );
 
 }
 function Circulo () 
 {
-	
+    let radio;
+    let perimetro;
+    let alambre;
+    const PI = 3.14
+    radio = parseInt(document.getElementById("txtIdRadio").value);
+    perimetro = 2*PI * radio;
+    alambre = perimetro * 3;
+
+    alert("Se necesitan " + alambre + " metros de alambre.");	
 }
 function Materiales () 
 {
