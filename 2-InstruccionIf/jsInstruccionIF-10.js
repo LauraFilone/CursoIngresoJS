20002c696f6b7d53c53dd20d49df5d44456ed996
unknown <laurafilone@gmail.com>
2021-07-14 17:08:55 -0300

    cambios

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let min = 1;
+	let max = 10;
 
+	let numero = Math.floor(Math.random()*(max-min+1)+min);
+
+	if(numero >= 9){
+		alert("excelente");
+	}else if (numero >=4){
+		alert("Aprobo");
+	}else{
+		alert("Vamos, la proxima se puede")
+	}
 }//FIN DE LA FUNCIÓN
